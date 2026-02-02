a5ed55535d2e90894365477deea392d25c6dbf53
Your Name <mohamad.coupani13651400@gmail.com>
2026-02-02 16:17:19 +0330

    Fix PWA manifest + Vercel SPA routing + icons v2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
-// If you are using MUI theme provider in your project, keep it.
-// Otherwise you can remove these imports safely.
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme/theme";
 
